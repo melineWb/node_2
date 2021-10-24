d8c6c7ea8a527d78b05548ff910bf15fa9489b32
Maryna Semiroz <Maryna_Semiroz@epam.com>
2021-10-24 10:05:20 +0300

    task 2.1

--- index.mjs
+++ index.mjs
@@ -2,8 +2,6 @@ import express from 'express';
 import createError from 'http-errors';
 import * as userHelpers from './userHelpers.mjs';
 
-console.log(userHelpers);
-
 const port = process.env.port || '3000';
 const app = express();
 const router = express.Router();
@@ -12,31 +10,83 @@ app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
 });
 
-router.param('user_id', function (req, res, next, id) {
-    const user = userHelpers.default.find((element) => {
-        return element.id === +id;
+router.param('user_id', (req, res, next, id) => {
+    let index;
+    const user = userHelpers.default.find((element, i) => {
+        const isCurrent = element.id === +id;
+        if (isCurrent) {
+            index = i;
+        }
+        return isCurrent;
     });
 
-    req.data = user ?? createError(404);
+    req.data = user ? { user, index } : createError(404);
     next();
+});
+
+app.use(express.json());
+
+router.route('/users/suggest').get((req, res) => {
+    let suggestedUsers = [];
+    if (req.query) {
+        suggestedUsers = userHelpers.getAutoSuggestUsers(req.query.login, req.query.limit);
+    }
+    res.send(suggestedUsers);
+});
+
+router.route('/users').get((req, res) => {
+    res.json(userHelpers.default);
 });
 
 router
     .route('/user/:user_id')
-    .get(function (req, res) {
-        res.json(req.data);
+    .get((req, res) => {
+        if (req.data?.user) {
+            res.json(req.data.user);
+        } else {
+            res.json(req.data);
+        }
     })
-    .put(function (req, res) {
-        req.user.name = req.params.name;
-        res.json(req.user);
+    .put((req, res) => {
+        if (req.data?.user) {
+            userHelpers.update(req.body, req.data);
+            res.status(204).json({
+                message: `User with id = ${req.params.user_id} successfully updated`,
+            });
+        } else {
+            userHelpers.create(req.body, req.params.user_id);
+            res.status(204).json({
+                message: `User with id = ${req.params.user_id} successfully created`,
+            });
+        }
     })
-    .post(function (req, res) {
-        console.log('b =' + req.body);
-        res.end();
+    .patch((req, res) => {
+        if (req.data?.user) {
+            userHelpers.update(req.body, req.data);
+            res.status(204).json({
+                message: `User with id = ${req.params.user_id} successfully updated`,
+            });
+        } else {
+            res.json(req.data);
+        }
     })
-    .delete(function (req, res, next) {
-        next(new Error('not implemented'));
+    .delete((req, res) => {
+        if (req.data?.user) {
+            const user = userHelpers.remove(req.data.user);
+            res.status(204).json({
+                message: `User with id = ${user.id} successfully removed`,
+            });
+        } else {
+            res.json(req.data);
+        }
     });
+
+router.route('/user').post((req, res) => {
+    userHelpers.create(req.body);
+    res.status(204).json({
+        message: `User with id = ${req.index} successfully created`,
+    });
+});
 
 app.use('/', router);
 
--- userHelpers.mjs
+++ userHelpers.mjs
@@ -1,20 +1,41 @@
 const users = [];
 import User from './user.mjs';
 
-users.push(
-    new User(users.length + 1, {
-        login: 'Hanna',
-        password: 'hnsdfkoe123',
-        age: 15,
-    }),
-);
-
-export function update() {
-    console.log(update);
+export function update(newData, data) {
+    if (data?.user) {
+        const updatedUSer = { ...data.user, ...newData };
+        users[data.index] = updatedUSer;
+        return updatedUSer;
+    } else {
+        return create(newData);
+    }
 }
 
-export function create() {
-    console.log(create);
+export function create(data, index) {
+    const newIndex = +index || users.length + 1;
+    const user = new User(newIndex, data);
+    users.push(user);
+    return user;
 }
+
+export function remove(user) {
+    if (user) {
+        user.isDeleted = true;
+        return user;
+    }
+}
+
+export function getAutoSuggestUsers(loginSubstring, limit = 1) {
+    return users
+        .filter((element) => element.login.includes(loginSubstring))
+        .sort((el1, el2) => el1.login.localeCompare(el2.login))
+        .slice(0, limit);
+}
+
+create({
+    login: 'Hanna',
+    password: 'hnsdfkoe123',
+    age: 15,
+});
 
 export default users;
