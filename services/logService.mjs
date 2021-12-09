5be5a94071049e099eb272990672855e4512c0c9
Maryna Semiroz <Maryna_Semiroz@epam.com>
2021-12-09 16:42:32 +0200

    remove commented code

--- services/logService.mjs
+++ services/logService.mjs
@@ -10,35 +10,14 @@ let logService = (req, res, next) => {
     const body = `Body: ${JSON.stringify(req.body)}`;
     const params = `Params: ${JSON.stringify(req.params)}`;
 
-    res.on('finish', function (err) {
+    res.on('finish', function () {
         const log = createLogStr(req.method, req.url, `${body} ${params}`, res.statusCode);
-        if (!err && /^20/.test(res.statusCode)) {
+        if (/^20/.test(res.statusCode)) {
             logger.info(log);
         } else {
-            if (err) {
-                logger.error(`[${utilService.getTime()}] logServiceError : ${err.message}`);
-            } else {
-                logger.error(createLogStr(req.method, req.url, `${body} ${params}`, res.statusCode));
-            }
+            logger.error(createLogStr(req.method, req.url, `${body} ${params}`, res.statusCode));
         }
     });
-
-    // res.on('finish', function () {
-    //     console.log(`FINISH ${res.statusCode} !!!`);
-    // });
-    // res.on('done', function () {
-    //     const log = `[${utilService.getTime()}] ${req.method}:${req.url} ${body} ${params} ${res.statusCode} ${duration}`;
-    //     console.log(`DONE ${res.statusCode} !!!`);
-
-    //     logger.info(log);
-    // });
-
-    // res.on('error', function () {
-    //     const log = `[${utilService.getTime()}] ${req.method}:${req.url} ${body} ${params} ${res.statusCode} ${duration}`;
-    //     console.log(` ERR ${res.statusCode} !!!`);
-
-    //     logger.error(log);
-    // });
 
     next();
 };
